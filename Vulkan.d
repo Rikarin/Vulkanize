75a01a17db5b2ae0566d8a1b70dbed3df02048f6
Satoshi <satoshi@gshost.eu>
2016-02-24 23:21:14 +0100

    Fix VK_DEFINE_NON_DISPATCHABLE_HANDLE

--- Vulkan.d
+++ Vulkan.d
@@ -47,7 +47,11 @@ private template VK_DEFINE_HANDLE(string name) {
 }
 
 private template VK_DEFINE_NON_DISPATCHABLE_HANDLE(string name) {
-    enum VK_DEFINE_NON_DISPATCHABLE_HANDLE = "struct " ~ name ~ "_T; alias " ~ name ~ " = " ~ name ~ "_T*;";
+    version (D_LP64) {
+        enum VK_DEFINE_NON_DISPATCHABLE_HANDLE = "struct " ~ name ~ "_T; alias " ~ name ~ " = " ~ name ~ "_T*;";
+    } else {
+        enum VK_DEFINE_NON_DISPATCHABLE_HANDLE = "alias " ~ name ~ " = ulong;";
+    }
 }
 
 mixin(VK_DEFINE_HANDLE!"VkInstance");
